c1b1ecefaf3f328f553d0bfc7a6660c4685dbd42
deborabastos <deborabastos14@gmail.com>
2023-01-27 23:15:44 -0300

    twitter api - retweet

--- Streaming Twitter_incompleto/tweetpy_deb_retweety.py
+++ Streaming Twitter_incompleto/tweetpy_deb_retweety.py
@@ -2,7 +2,6 @@
 
 #importa bibliotecas
 import tweepy as tw
-import time
 import os
 from dotenv import load_dotenv
 load_dotenv()
@@ -21,34 +20,33 @@ client = tw.Client(token, consumer_key, consumer_secret, access_token, access_to
 auth = tw.OAuth1UserHandler(consumer_key, consumer_secret, access_token, access_token_secret)
 api = tw.API(auth)
 
-
-#cria lista de termos a serem buscados
-search_terms = ["data science", "ciência de dados"]
-
+#para pegar uma lista de tweets, acionar streaming
 #cria uma classe MyStream filha da classe StreamClient do tweetpy
-#essa classe será usada futuramente para abrir a transmissão (stream)
-#usaremos algumas funções herdadas da StreamClient
 
 class MyStream(tw.StreamingClient):
-    def on_connect(self):               #roda quando o stream começa a rodar 
+    def on_connect(self):               #informa que o streaming começou
         print("Connected")
 
     def on_tweet(self, tweet):          #é chamada quando um novo tweet que atende aos critérios estabalececidos é detectado
-        if tweet.referenced_tweets == None:
-            print(tweet.text)
+        print(tweet.text)
+        try:
+            client.retweet(tweet.id)
+        
+        except Exception as error:
+            print(error)
 
-            time.sleep(0.2)
-    
+
 #cria uma instância da classe MyStream
-stream = MyStream(bearer_token=token)
+stream = MyStream(bearer_token=token)    
 
-#adicionado os termos indicados na lista à regra de filtragem
-#IMPORTANTE: as regras não desaparecem quando encerra o bot. Para deletar um regra, é necessário usar detele_rules().
-#para ver a lista de regras ativas, usar get_rules()
-for term in search_terms:
-    stream.add_rules(tw.StreamRule(term))
+#Criar StreamRules.
+#Regras que o stream vai seguir ao retornar o resultado
+#Nesta regra: retornar os tweets que contenham #python OU #datascience E nào sejam retweet, nem reply
+rule = tw.StreamRule("(#cienciadedados OR #datascience) (-is:retweet -is:reply)")
 
+#Para adicionar a regra ao Stream
+#O parâmetro dry_run=True impede que a regra se mantenha válida após finalizado o script
+stream.add_rules(rule, dry_run=True)
 
-#Para começar o streaming e filtrar os termos
-stream.filter(tweet_fields=["referenced_tweets"])
-
+#Para rodar o stream
+stream.filter()
